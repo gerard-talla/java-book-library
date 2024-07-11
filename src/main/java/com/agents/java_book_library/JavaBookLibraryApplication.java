ef7be8bb6f8e3bf4f03466d1ea70d9a755c662da
Gerard Talla <gerard.tchano@gmail.com>
2024-07-11 14:50:33 +0200

    initial commit

---
+++ src/main/java/com/agents/java_book_library/JavaBookLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.agents.java_book_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBookLibraryApplication.class, args);
+	}
+
+}
