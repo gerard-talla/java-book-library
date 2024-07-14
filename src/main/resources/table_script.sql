93eb1e1ca1b383502b65c3c81f5a45bbd57fff0d
Gerard Talla <gerard.tchano@gmail.com>
2024-07-15 00:53:15 +0200

    typo refactoring

--- src/main/resources/table_script.sql
+++ src/main/resources/table_script.sql
@@ -1,32 +1,32 @@
 CREATE TABLE Author (
-                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        authorId BIGINT AUTO_INCREMENT PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
                         dateOfBirth VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Book (
-                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      bookId BIGINT AUTO_INCREMENT PRIMARY KEY,
                       title VARCHAR(255) NOT NULL,
                       genre VARCHAR(255) NOT NULL,
                       price DECIMAL(10, 2) NOT NULL,
                       author_id BIGINT NOT NULL,
-                      CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Author(id)
+                      CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES Author(authorId)
 );
 
 CREATE TABLE Member (
-                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        memberId BIGINT AUTO_INCREMENT PRIMARY KEY,
                         username VARCHAR(255) UNIQUE NOT NULL,
                         email VARCHAR(255) NOT NULL,
                         address VARCHAR(255) NOT NULL,
                         phoneNumber VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Loan (
-                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE LoanBook (
+                      LoanBookId BIGINT AUTO_INCREMENT PRIMARY KEY,
                       lendDate DATE NOT NULL,
                       returnDate DATE NOT NULL,
                       book_id BIGINT NOT NULL,
                       member_id BIGINT NOT NULL,
-                      CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Book(id),
-                      CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES Member(id)
+                      CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES Book(bookId),
+                      CONSTRAINT fk_member FOREIGN KEY (member_id) REFERENCES Member(memberId)
 );
